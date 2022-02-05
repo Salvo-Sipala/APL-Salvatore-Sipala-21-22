50191c494463db5cebf47f4181d620cd8da6d58c
Salvo-Sipala <salvo91s@hotmail.it>
2022-02-05 10:56:46 +0100

    Try tunneling

---
+++ PersonalStockMarcket/C#-FrontEnd/FrontEnd/Models/NgrokTunnel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FrontEnd.Models
+{
+    public class NgrokTunnel
+    {
+
+        public string Ngrok { get; set; }
+
+        public NgrokTunnel (string ngrok)
+        {
+            Ngrok = ngrok;
+        }
+            
+    }
+}
